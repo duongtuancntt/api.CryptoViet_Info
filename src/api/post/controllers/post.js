ded02c52515084d8294914b77792189e8467c645
Duong Tuan <duongtuan12a11@gmail.com>
2023-04-13 18:02:48 +0700

    first commit

---
+++ src/api/post/controllers/post.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * post controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+(module.exports = createCoreController("api::post.post")),
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id } = ctx.params;
+
+      const entity = await strapi.db.query("api::post.post").findOne(id);
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity, { pagination });
+    },
+  });
